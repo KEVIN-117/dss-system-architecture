134ba4bec178902d3a52ea44d6e1c595ed8f3fa6
KEVIN-117 <krodriguezcondori34@gmail.com>
2026-08-12 17:20:26 -0400

    feat(db): consolidate config package and fix Flyway tests

---
+++ auth-service/src/main/java/uatf/dss/authservice/configuration/FlywayConfig.java
@@ -0,0 +1,26 @@
+package uatf.dss.authservice.configuration;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class FlywayConfig implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof DataSource) {
+            Flyway.configure()
+                    .dataSource((DataSource) bean)
+                    .locations("classpath:db/migration")
+                    .baselineOnMigrate(true)
+                    .load()
+                    .migrate();
+        }
+        return bean;
+    }
+}
